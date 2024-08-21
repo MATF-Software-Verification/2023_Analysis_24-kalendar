fd6d3c860900604cc5e46e07212fd538aca6ab7e
jovanaMATF <jovanaadzic42@gmail.com>
2024-08-21 22:39:08 +0200

    Remove old callgrind folder

--- callgrind/callgrind.sh
+++
@@ -1,6 +0,0 @@
-set -xe
-
-valgrind --tool=callgrind $@ --log-file="$(date +%s).callgrind.out" /mnt/c/Users/adzic/Desktop/vs/2023_Analysis_24-kalendar/2023_Analysis_24-kalendar/24-kalendar/build-calendar-Desktop-Debug/calendar
-
-latest_file=$(ls -t cachegrind.out.* | head -n1)
-cg_annotate "$latest_file" > "$latest_file.txt"
