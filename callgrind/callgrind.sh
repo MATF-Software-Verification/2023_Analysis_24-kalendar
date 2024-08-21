6a486260b7003f6c6b39a60670e92a7c2ddc72de
jovanaMATF <jovanaadzic42@gmail.com>
2024-08-21 22:40:38 +0200

    Add new callgrind tool folder

---
+++ callgrind/callgrind.sh
@@ -0,0 +1,3 @@
+valgrind --tool=callgrind --smc-check=stack --keep-debuginfo=yes --callgrind-out-file=callgrind.out --log-file="callgrind-%p.txt" --dump-instr=yes --collect-jumps=yes --simulate-cache=yes /home/jovana/Desktop/vs/2023_Analysis_24-kalendar/19-under-the-c/UNDER_THE_C/build/Desktop_Qt_5_15_1_GCC_64bit-Debug/UNDER_THE_C
+
+
