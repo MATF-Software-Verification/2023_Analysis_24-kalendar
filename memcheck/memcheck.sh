a258ce80c2f36930ea891f49f73c6c9b964da76c
jovanaMATF <jovanaadzic42@gmail.com>
2024-08-22 23:04:09 +0200

    Update memcheck script

--- memcheck/memcheck.sh
+++ memcheck/memcheck.sh
@@ -1,3 +1,5 @@
 set -xe
 
-valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose $@ --log-file="$(date +%s).memcheck.out" /home/jovana/Desktop/vs/2023_Analysis_24-kalendar/19-under-the-c/UNDER_THE_C/build/Desktop_Qt_5_15_1_GCC_64bit-Debug/UNDER_THE_C
+valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file="$(date +%s).memcheck.out" ../19-under-the-c/UNDER_THE_C/build/Desktop_Qt_5_15_1_GCC_64bit-Debug/UNDER_THE_C
+
+echo "finished memcheck"
