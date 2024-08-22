59fb6ed6d60262ecc815fb5110bad90c4084babc
jovanaMATF <jovanaadzic42@gmail.com>
2024-08-23 00:10:52 +0200

    Update callgrind script Add new report files for callgrind

--- callgrind/callgrind.sh
+++ callgrind/callgrind.sh
@@ -1,3 +1,5 @@
-valgrind --tool=callgrind --smc-check=stack --keep-debuginfo=yes --callgrind-out-file=callgrind.out --log-file="callgrind-%p.txt" --dump-instr=yes --collect-jumps=yes --simulate-cache=yes /home/jovana/Desktop/vs/2023_Analysis_24-kalendar/19-under-the-c/UNDER_THE_C/build/Desktop_Qt_5_15_1_GCC_64bit-Debug/UNDER_THE_C
+set xe
 
+valgrind --tool=callgrind  --keep-debuginfo=yes --callgrind-out-file="callgrind-%p.out" --log-file="callgrind-%p.txt" --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ../19-under-the-c/UNDER_THE_C/build/Desktop_Qt_5_15_1_GCC_64bit-Profile/UNDER_THE_C
 
+echo "finished callgrind"
